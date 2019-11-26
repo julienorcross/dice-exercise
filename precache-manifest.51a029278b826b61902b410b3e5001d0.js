32aa7ea9fdebc4d57aafeff5a655fdc6e68ac5c7
Julie Norcross <julesnorc@gmail.com>
2019-11-26 11:32:48 -0500

    Updates

---
+++ precache-manifest.51a029278b826b61902b410b3e5001d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd183128fe0c1a03228cf9b2e5bc005",
+    "url": "/dice-exercise/index.html"
+  },
+  {
+    "revision": "3c7f99312e68bfc1a607",
+    "url": "/dice-exercise/static/css/main.03602889.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/dice-exercise/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3c7f99312e68bfc1a607",
+    "url": "/dice-exercise/static/js/main.e201d0c5.chunk.js"
+  },
+  {
+    "revision": "58f6d1a9e6245402c76d",
+    "url": "/dice-exercise/static/js/runtime~main.1e9039e7.js"
+  }
+]);
